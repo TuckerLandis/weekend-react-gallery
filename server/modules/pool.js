d2e64879c79fe9ec77d3ff93a709aebaa39900ce
TuckerLandis <landistuckerc@gmail.com>
2021-06-06 18:23:57 -0500

    explicit local host

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -99,11 +99,11 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
 
-    // database: 'react_gallery', // database name (this will change)
-    // host: 'localhost', // where to find the database
-    // port: 5432,        // port for finding the database
-    // max: 10,           // max number of connections for the pool
-    // idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
+    database: 'react_gallery', // database name (this will change)
+    host: '0.0.0.0', // where to find the database
+    port: 5432,        // port for finding the database
+    max: 10,           // max number of connections for the pool
+    idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
 });
 
 // Listener setup on the pool isn't required, 
