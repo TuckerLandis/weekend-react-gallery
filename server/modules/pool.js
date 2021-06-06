e32dfb31404aae01f027e24c6d82fe6d7bb9dc93
TuckerLandis <landistuckerc@gmail.com>
2021-06-06 17:48:36 -0500

    can bind port

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,33 @@
 // Node Module that will connect to postgesql
 const pg = require('pg');
+let config = {};
 
 // Setup PG to connect to the database
 const Pool = pg.Pool;
 
-const pool = new Pool({
-    database: 'react_gallery', // database name (this will change)
-    host: 'localhost', // where to find the database
-    port: 5432,        // port for finding the database
-    max: 10,           // max number of connections for the pool
-    idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
-});
+
+if (process.env.DATABASE_URL) {
+  config = {
+    // We use the DATABASE_URL from Heroku to connect to our DB
+    connectionString: process.env.DATABASE_URL,
+    // Heroku also requires this special `ssl` config
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  // If we're not on heroku, configure PG to use our local database
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'react_gallery', // CHANGE THIS LINE to match your local database name!
+  };
+
+// const pool = new Pool({
+//     database: 'react_gallery', // database name (this will change)
+//     host: 'localhost', // where to find the database
+//     port: 5432,        // port for finding the database
+//     max: 10,           // max number of connections for the pool
+//     idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
+// });
 
 // Listener setup on the pool isn't required, 
 // but can be super handy for troubleshooting.
